a9431e757a8cbb15836d94f048908429222ee910
Alisa Mordvinova <87349480+QAurum@users.noreply.github.com>
2026-02-11 20:38:17 +0300

    Update test_parser.py
    Загрузила предыдущее задание

--- test_parser.py
+++ test_parser.py
@@ -2,11 +2,19 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-# инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
+# В файл test_items.py напишите тест, который проверяет, что страница товара на сайте содержит кнопку добавления в корзину.
 
-link = "http://selenium1py.pythonanywhere.com/"
+def test_user_should_see_add_to_basket_button(browser):
+    print("\nОткрываем страницу товара..")
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    
+    try:
+        print("\nОжидаем пока кнопка не станет доступной..")
+        button_add = WebDriverWait(browser, 5).until(
+            EC.element_to_be_clicable((By.CSS_SELECTOR, "button.btn-add-to-basket"))
+    )
 
-
-def test_guest_should_see_login_link(browser):
-    browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#login_link")
+        assert button_add.is_displayed(), "Кнопка не видна/не отображается"
+    
+    except NoSuchElementExeption:
+        assert False, "Кнопка не найдена"
